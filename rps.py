26644c2a08ed07e7bf036560ed06f194edcd4645
Arun Moorthy <arunmoorthy@DN51ucp1.SUNet>
2024-02-15 17:17:01 -0800

    instructions

--- rps.py
+++ rps.py
@@ -1,5 +1,11 @@
+import random
+
 def main():
     print("Hello! Welcome to Rock, Paper, Scissors!")
+    print("The rules of this game are as follows: ")
+    print("You will choose rock, paper, or scissors. The program will also select either rock, paper, or scissors.")
+    print("The goal is to beat the computer. Rock beats scissors. Scissors beats paper. Paper beats rock.")
+    print("If you have the same selection, the game is a tie.")
 
 if __name__ == "__main__":
     main()
