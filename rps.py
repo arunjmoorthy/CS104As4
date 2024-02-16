dbab6ccd2e184830f1b42eafeddd472710e9a5d0
Arun Moorthy <arunmoorthy@DN51ucp1.SUNet>
2024-02-15 17:28:40 -0800

    game with no rematches

--- rps.py
+++ rps.py
@@ -7,5 +7,16 @@ def main():
     print("The goal is to beat the computer. Rock beats scissors. Scissors beats paper. Paper beats rock.")
     print("If you have the same selection, the game is a tie.")
 
+    user = input("Enter rock, paper, or scissors: ")
+    list = ["rock", "paper", "scissors"]
+    computer = random.choice(list)
+
+    if(user == computer):
+        print("Tie!")
+    elif((user == "rock" and computer == "scissors") or (user == "scissors" and computer == "paper") or (user == "paper" and computer == "rock")):
+        print("You win!")
+    else:
+        print("Oh no! You lose!")
+
 if __name__ == "__main__":
     main()
